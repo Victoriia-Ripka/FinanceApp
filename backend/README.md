4cd08eb54572eae8a131a5903c439bd647b24cab
Victoriia-Ripka <vikaripka@gmail.com>
2024-12-20 09:45:31 +0100

    backend init

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,7 @@
-node app.js - run the project
+при локальному запуску спочатку *npm i*
+run the project *npm run start*
+
+`http://localhost:8080` 
+/api/users/register/ - {name, email, password, currency, referalCode*} -> token видається при успішній реєстраціі
+/api/users/login/ - {email, password} -> ???
+/api/users/logout/ - {email} 
---
+++ backend/models/user.js
@@ -0,0 +1,58 @@
+import { Schema, model } from "mongoose";
+import Joi from "joi";
+import { handleMongooseError } from "../helpers/index.js";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for user"],
+    },
+    email: { //login
+      type: String,
+      required: [true, "Email is required (example@example.com)"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Set password for user (examplepassword)"],
+      minlength: 6,
+    },
+    currency: {
+      type: String,
+      required: [true, "Set currency for account"],
+    },
+    referalCode: {
+      type: String,
+      required: [false],
+    },
+    token: {
+      type: String,
+    }
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  password: Joi.string().min(6).required(),
+  currency: Joi.string().required(),
+  referalCode: Joi.string(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().min(6).required(),
+});
+
+const schemas = {
+  registerSchema,
+  loginSchema,
+};
+
+const User = model("user", userSchema);
+
+export { User, schemas };
