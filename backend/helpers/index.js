32ad63bb2c610bd5aef17260ce67081f1c23227e
Victoriia-Ripka <vikaripka@gmail.com>
2024-12-20 21:03:08 +0100

    .

--- backend/helpers/index.js
+++ backend/helpers/index.js
@@ -1,5 +1,5 @@
 import HttpError from "./HttpError.js";
 import ctrlWrapper from "./CtrlWrapper.js";
-import handleMongooseError from "./handleMongooseError.js";
+import handleMongooseError from "./HandleMongooseError.js";
 
 export { HttpError, ctrlWrapper, handleMongooseError };
